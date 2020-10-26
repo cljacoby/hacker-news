a6460ccb118bb209f552aa0fd5645a20386c8786
cljacoby <cljacoby@gmail.com>
2020-10-26 15:47:33 -0400

    Chose strict API model definition
    Opted to go with struct API model struct definitions. See the last
    commit message for more on the loose vs. strict structure I suggested.
    I'm also considering uniting all the API struct definitions by a parent
    enum, considering that in the HackerNews API documentation, it simply
    uses a single HTTP operation called `Get Item By Id`

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
 use std::error::Error;
 
-use reqwest::blocking::Client;
 use reqwest::Method;
-use serde::{Deserialize, Serialize};
-use serde_with::rust::double_option;
-
-pub mod loose;
-pub mod strict;
+use serde::{self, Deserialize, Serialize};
 
 pub struct HNClient {
     // Using blocking for now becase its quick and easy. Consider adding async client.
@@ -22,16 +17,188 @@ impl HNClient {
         Self { client }
     }
 
-    #[rustfmt::skip]
-    pub fn get_story_by_id(&self, id: &str) -> Result<loose::Story, Box<Error>> {
-        let url = format!("{base}/v0/item/{id}.json?print=pretty", base = HNClient::BASE_URL, id = id);
-        let story = self.client
-            .request(Method::GET, &url)
-            .send()?
-            .json()?;
-        
-        Ok(story)
-    }
+
+    pub fn get_by_id() {}
+
+
+    // #[rustfmt::skip]
+    // pub fn get_story_by_id(&self, id: &str) -> Result<Base, Box<dyn Error>> {
+    //     let url = format!("{base}/v0/item/{id}.json?print=pretty", base = HNClient::BASE_URL, id = id);
+    //     let story = self.client
+    //         .request(Method::GET, &url)
+    //         .send()?
+    //         .json()?;
+    //     
+    //     Ok(story)
+    // }
+}
+
+
+/*
+ * TODO:
+ * The struct `Base` was defined by directly referencing the model outlined in this
+ * documentation:
+ *
+ * https://hackernews.api-docs.io/v0/items/base
+ *
+ * I'm not sure if the struct is itself that useful. What may be useful is having a
+ * catch-all superset of all the Item variants. This could also use the Serde double_option,
+ * and have the varaints have try_into methods to deive them.
+ * */
+
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Base {
+    /// The item's unique id.
+    pub id: i64,
+    /// true if the item is deleted.
+    pub deleted: bool,
+    /// The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+    #[serde(rename = "type")]
+    pub hn_type: String,
+    /// The username of the item's author.
+    pub by: String,
+    /// Creation date of the item, in Unix Time.
+    pub time: i64,
+    /// The comment, story or poll text. HTML.
+    pub text: String,
+    /// true if the item is dead.
+    pub dead: bool,
+    /// The comment's parent: either another comment or the relevant story.
+    pub parent: i64,
+    /// The pollopt's associated poll.
+    pub poll: String,
+    /// The ids of the item's comments, in ranked display order.
+    pub kids: Vec<i64>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Job {
+    /// The item's unique id.
+    pub id: i64,
+    /// true if the item is deleted.
+    pub deleted: bool,
+    /// The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+    #[serde(rename = "type")]
+    pub hn_type: String,
+    /// The username of the item's author.
+    pub by: String,
+    /// Creation date of the item, in Unix Time.
+    pub time: i64,
+    /// true if the item is dead.
+    pub dead: bool,
+    /// The ids of the item's comments, in ranked display order.
+    pub kids: Vec<i64>,
+    /// The comment, story or poll text. HTML.
+    pub text: String,
+    /// The URL of the story.
+    pub url: String,
+    /// The title of the story, poll or job.
+    pub title: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Story {
+    /// The item's unique id.
+    pub id: i64,
+    /// true if the item is deleted.
+    pub deleted: bool,
+    /// The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+    #[serde(rename = "type")]
+    pub hn_type: String,
+    /// The username of the item's author.
+    pub by: String,
+    /// Creation date of the item, in Unix Time.
+    pub time: i64,
+    /// true if the item is dead.
+    pub dead: bool,
+    /// The ids of the item's comments, in ranked display order.
+    pub kids: Vec<i64>,
+    /// In the case of stories or polls, the total comment count.
+    pub descendants: i64,
+    /// The story's score, or the votes for a pollopt.
+    pub score: i64,
+    /// The title of the story, poll or job.
+    pub title: String,
+    /// The URL of the story.
+    pub url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Comment {
+    /// The item's unique id.
+    pub id: i64,
+    /// true if the item is deleted.
+    pub deleted: bool,
+    /// The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+    #[serde(rename = "type")]
+    pub hn_type: String,
+    /// The username of the item's author.
+    pub by: String,
+    /// Creation date of the item, in Unix Time.
+    pub time: i64,
+    /// true if the item is dead.
+    pub dead: bool,
+    /// The ids of the item's comments, in ranked display order.
+    pub kids: Vec<i64>,
+    /// The comment's parent: either another comment or the relevant story.
+    pub parent: i64,
+    /// The comment, story or poll text. HTML.
+    pub text: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Poll {
+    /// The item's unique id.
+    pub id: i64,
+    /// true if the item is deleted.
+    pub deleted: bool,
+    /// The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+    #[serde(rename = "type")]
+    pub hn_type: String,
+    /// The username of the item's author.
+    pub by: String,
+    /// Creation date of the item, in Unix Time.
+    pub time: i64,
+    /// true if the item is dead.
+    pub dead: bool,
+    /// The ids of the item's comments, in ranked display order.
+    pub kids: Vec<i64>,
+    /// A list of related pollopts, in display order.
+    pub parts: Vec<i64>,
+    /// In the case of stories or polls, the total comment count.
+    pub descendants: i64,
+    /// The story's score, or the votes for a pollopt.
+    pub score: i64,
+    /// The title of the story, poll or job.
+    pub title: String,
+    /// The comment, story or poll text. HTML.
+    pub text: String,
+
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct PollOption {
+    /// The item's unique id.
+    pub id: i64,
+    /// true if the item is deleted.
+    pub deleted: bool,
+    /// The type of item. One of "job", "story", "comment", "poll", or "pollopt".
+    #[serde(rename = "type")]
+    pub hn_type: String,
+    /// The username of the item's author.
+    pub by: String,
+    /// Creation date of the item, in Unix Time.
+    pub time: i64,
+    /// true if the item is dead.
+    pub dead: bool,
+    /// The ids of the item's comments, in ranked display order.
+    pub kids: Vec<i64>,
+    /// The comment's parent: either another comment or the relevant story.
+    pub parent: i64,
+    /// The story's score, or the votes for a pollopt.
+    pub score: i64,
+    
 }
 
 #[cfg(test)]
@@ -40,9 +207,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_request() -> Result<(), Box<std::error::Error>> {
-        // TODO: Get a story and assert_eq! against expected value
-        Ok(())
+    fn test_full_init() {
+        let story = Base {
+            id:         100,
+            deleted:    false,
+            hn_type:    "hn_type".to_string(),
+            by:         "author".to_string(),
+            time:       0,
+            text:       "text".to_string(),
+            dead:       false,
+            parent:     0,
+            poll:       "poll".to_string(),
+            kids:       vec![1],
+        };
+        println!("story = {:#?}", story);
+        let s = serde_json::to_string(&story);
+        println!("s = {:#?}", s);
     }
 
+    #[test]
+    fn test_default() {
+        let story = Base {
+            id: 100,
+            ..Base::default()
+        };
+        println!("story = {:#?}", story);
+        let s = serde_json::to_string(&story);
+        println!("s = {:#?}", s);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use clap::App;
 use clap::AppSettings;
 use clap::Arg;
 
-fn main() -> Result<(), Box<Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let app = App::new("hnews")
         .setting(AppSettings::ArgRequiredElseHelp)
         .arg(
@@ -23,28 +23,7 @@ fn main() -> Result<(), Box<Error>> {
         .expect("Error: Received no id value.");
 
     let client = hnews::HNClient::new();
-    let story = client.get_story_by_id(id)?;
-    story.print();
-
-    let res = match story.kids {
-        None => {
-            println!("{}'s kids field was absent.", id);
-        },
-        Some(v) => match v {
-            None => {
-                println!("{}'s kids field was null.", id);
-            },
-            Some(kids) => { 
-                for kid in kids.iter() {
-                    let id = &format!("{}", kid);
-                    // TODO: The abstraction here is wrong, as the instanes of `kid` are Comments,
-                    // not Stories
-                    let story = client.get_story_by_id(id)?;
-                    story.print();
-                }
-            }
-        }
-    };
+    let _story = client.get_story_by_id(id)?;
     
     Ok(())
 }
