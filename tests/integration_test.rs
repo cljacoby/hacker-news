041395f6f1cf6dda96be4b05087f50bf49cc40b9
cljacoby <cljacoby@gmail.com>
2020-10-27 22:41:52 -0400

    Add integration test
    Added an integration test. The test simply takes Hacker News JSON dump
    files and attempts to parse all the response messages. Currently failing
    on deleted messages, which I'm still thinking about how to handle.
    They're problematic because they lack almost all expected response
    fields including the key `type`, which serde uses to figure out which
    enum variant to parse to.

---
+++ tests/integration_test.rs
@@ -0,0 +1,68 @@
+use std::error::Error;
+use std::fs;
+use std::fmt;
+use std::fs::File;
+use std::path::Path;
+use std::io::BufReader;
+use hnews::{Item};
+use serde_json;
+use serde_json::Value;
+
+const TEST_DATA_DIR: &str = "./data";
+
+#[derive(Debug)]
+struct TestError {
+    msg: String,
+}
+
+impl fmt::Display for TestError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "msg: {}", self.msg) 
+    }
+}
+
+impl Error for TestError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[test]
+fn integration_test() -> Result<(), Box<dyn Error>> {
+
+    let mut err_count = 0;
+    let test_data_dir = Path::new(TEST_DATA_DIR);
+
+    for entry in fs::read_dir(test_data_dir)? {
+
+        let entry = entry?;
+        let path = entry.path();
+        let f = File::open(path)?;
+        let reader = BufReader::new(f);
+        
+        // This is not performant. I'm parsing twice, and cloning a value in the
+        // middle. However, parsing the JSON first to Values, and then in to
+        // the custom hnews::Item type allows each parse to be handled independently.
+        // Otherwise, if you use serde_json to parse directly to a Vec<hnews::Item>,
+        // the entire parse will fail at the first error.
+
+        let values: Vec<Value> = serde_json::from_reader(reader)?;
+        for v in values.iter() {
+            match serde_json::from_value::<Item>(v.clone()) {
+                Ok(_) => continue,
+                Err(err) => {
+                    err_count += 1;
+                    println!("error = {:?}. value = {:?}", err, v);
+                },
+            }
+        }
+
+    }
+
+    match err_count {
+        0 => Ok(()),
+        _ => Err(Box::new(TestError {
+            msg: format!("Integrationt test failed with {} messages failed to parse", err_count),
+        })),
+    }
+}
