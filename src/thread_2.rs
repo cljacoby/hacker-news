3e5bcbd327c0bdcdaee00a67cdcbf098390eb14c
cljacoby <cljacoby@gmail.com>
2020-11-14 19:57:55 -0500

    Implement simpler comment thread iteration
    I'm trying a much simpler API to lazily iterate over a comment tree.
    Originally I thought using a VecDeque<Id> was less preferable than
    trying to Enqueu Story and Comment instances. I thought I could use the
    type system for effectively to make better compile time checks. But ...
    considering it's all API data coming back from the server, there's
    really no guarantees as to what comes over the wire. So basically the
    resolution is that the Iterator itself returns Results in each execution
    of `next()`.

---
+++ src/thread_2.rs
@@ -0,0 +1,62 @@
+use std::collections::VecDeque;
+use std::error::Error;
+
+use crate::HNClient;
+use crate::HNError;
+use crate::models::Id;
+use crate::models::Item;
+use crate::models::Comment;
+
+struct ThreadIter {
+    queue: VecDeque<Id>,
+    client: Box<HNClient>,
+}
+
+impl Iterator for ThreadIter {
+    type Item = Result<Comment, Box<dyn Error>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+        // Pop Id from queue, return sentinal None if queue is exhausted
+        let id = match self.queue.pop_front() {
+            None => return None,
+            Some(id) => id,
+        };
+
+        // Make request to HackerNews API to get this Id's Comment data
+        let resp = self.client.get_by_id(id);
+        let comment = match resp {
+
+            // If API request failed, return Error
+            Err(src) => {
+                let err = HNError::new(
+                    format!("Request to HackerNews API failed"),
+                    Some(src),
+                );
+                return Some(Err(Box::new(err))); 
+            },
+
+            // If API we got an Item, verify that it's a Comment variant, and extract inner Commnent
+            Ok(item) => match item {
+                Item::Comment(comment) => comment,
+                _ => {
+                    let err = HNError::new(
+                        format!("Thread iteration got non-Comment Item variant"),
+                        None,
+                    );
+                    return Some(Err(Box::new(err))); 
+                }
+            } 
+        };
+
+        // Enqueue this comment's childern comments 
+        if let Some(ref kids) = comment.kids {
+            for kid in kids {
+                self.queue.push_back(*kid);
+            }
+        }
+
+        Some(Ok(comment))
+    }
+}
+
