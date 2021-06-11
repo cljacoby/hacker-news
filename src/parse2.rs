9f174634732d83e045def0c9947befeec092fc7d
cljacoby <cljacoby@gmail.com>
2021-06-11 19:31:29 -0400

    Progress on better HTML parsing pattern
    I can't think of a nice/easy way to define either a trait or generic
    function to provide the better abstraction for HTML parsing. Maybe I'm
    not clever enough. Hopefully it doesn't occur to me immediately after I
    commit to this refactor.
    
    I'm planning to simply use a module convention where I break apart the
    huge procedural style I'm using to parse HTML into separate methods.
    Each methods will essentially extract a given field of the struct.
    
    Also I just realized I need to refactor the error type I defined way
    back.

--- src/parse2.rs
+++ src/parse2.rs
@@ -4,8 +4,12 @@ use std::error::Error;
 // use crate::error::HNError;
 use crate::model::Id;
 use scraper::Html;
+// use scraper::element_ref::Select;
+use scraper::html::Select;
 use scraper::Selector;
 use lazy_static::lazy_static;
+use crate::model::Comment;
+use crate::error::HNError;
 
 
 /// ```rust 
@@ -20,29 +24,34 @@ use lazy_static::lazy_static;
 /// }
 /// ```
 
-// impl TryFrom<&Html> for Comment {
-//     type Error = HNError;
-//     fn try_from(html: &Html) -> Result<Self, Self::Error> {
-//         unimplemented!()
-//     }
-// }
-// pub(crate) trait FromHtml {
-//     fn from_html(html: &Html) -> Result<Self, Box<dyn Error>> where Self: Sized;
-// }
-
 pub mod CommentParser {
 
     use super::*;
 
     lazy_static! {
+
         // Applied to root of HTML document
-        static ref qs_comment_tree: Selector = Selector::parse("table.comment-tree").unwrap();
+        static ref qs_comment_table: Selector = Selector::parse("table.comment-tree").unwrap();
+        
         // Applied to comment tree root (i.e. node `table.comment-tree`)
         static ref qs_comment: Selector = Selector::parse("tr.athing.comtr").unwrap();
+        
         // Applied to comment node (i.e. node `tr.athing.comtr`)
         static ref qs_comment_text: Selector = Selector::parse("span.commtext").unwrap();
         static ref qs_comment_user: Selector = Selector::parse("a.hnuser").unwrap();
         static ref qs_indent: Selector = Selector::parse("td.ind img").unwrap();
+    }
+
+    // "main" method
+    fn parse(html: &Html) -> Result<Comment, Box<dyn Error>> {
+        unimplemented!()
+    }
+
+    fn query_comment_nodes(html: &Html) -> Result<Select, Box<dyn Error>> {
+        // let root html.select(&qs_comment_table)
+        //     .next()
+        //     .ok_or(HNError::)
+        unimplemented!()
     }
 
     fn extract_id(html: &Html) -> Result<Id, Box<dyn Error>> {
