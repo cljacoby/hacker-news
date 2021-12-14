e7ece7ec8e59eb59043dec04fd3caca93e03ff0c
cljacoby <cljacoby@gmail.com>
2021-12-13 22:13:36 -0500

    Got async/await basic implementation to compile
    Last commit message may have been overly grumpy in tone. Had spent too
    long scratching my head with compile messages I wasn't making good sense
    of. Eventually discovered that reqwest and tokio need to have somewhat
    aligned versions, and if they aren't compatible reqwest panics as if
    tokio is not present.
    
    I still commented out major portions of the code, and will need to
    re-introduce those before merging.

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,20 @@ use std::error::Error;
 use hacker_news::cli::HnCommand;
 use hacker_news::cli::hacker_news::HackerNews;
 use hacker_news::util::init_logger;
-use tokio;
 
-// async fn main() {
-// #[tokio::main]
-// #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn Error>> {
-//     init_logger();
-// 
-// 
-//     let app = HackerNews::parser();
-//     let matches = app.get_matches_from(env::args_os());
-// 
-//     let _res = match HackerNews::cmd(&matches).await {
-//         Ok(_) => std::process::exit(0),
-//         Err(e) => {
-//             println!("{}", e);
-//             std::process::exit(1);
-//         }
-//     };
-// }
+#[tokio::main(core_threads = 4)]
+async fn main() -> Result<(), Box<dyn Error>> {
+    init_logger();
 
-fn main() {
-    // tokio::runtime::Builder::new_multi_thread()
-    tokio::runtime::Builder::new_current_thread()
-        .worker_threads(2)
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
-            let app = HackerNews::parser();
-            let matches = app.get_matches_from(env::args_os());
-            let _res = match HackerNews::cmd(&matches).await {
-                Ok(_) => std::process::exit(0),
-                Err(e) => {
-                    println!("{}", e);
-                    std::process::exit(1);
-                }
-            };
 
-        })
+    let app = HackerNews::parser();
+    let matches = app.get_matches_from(env::args_os());
+
+    let _res = match HackerNews::cmd(&matches).await {
+        Ok(_) => std::process::exit(0),
+        Err(e) => {
+            println!("{}", e);
+            std::process::exit(1);
+        }
+    };
 }
