6971c6820eba6b3f3e569d11924da589afb5ac98
cljacoby <cljacoby@gmail.com>
2021-12-29 09:21:58 -0500

    Got basic HN front page TUI working

--- src/cli/browse.rs
+++ src/cli/browse.rs
@@ -1,9 +1,13 @@
-use std::error::Error;
+use crate::cli::HnCommand;
+use crate::client::html_client::Client;
+use crate::model::Listing;
+
 use clap::App;
 use clap::ArgMatches;
 use clap::SubCommand;
-use crate::cli::HnCommand;
 
+use std::error::Error;
+use std::io;
 
 use crossterm::event;
 use crossterm::event::DisableMouseCapture;
@@ -16,9 +20,6 @@ use crossterm::terminal::enable_raw_mode;
 use crossterm::terminal::EnterAlternateScreen;
 use crossterm::terminal::LeaveAlternateScreen;
 
-
-use std::io;
-
 use tui::backend::Backend;
 use tui::backend::CrosstermBackend;
 use tui::layout::Alignment;
@@ -29,90 +30,142 @@ use tui::style::Color;
 use tui::style::Modifier;
 use tui::style::Style;
 use tui::text::Span;
+use tui::text::Spans;
 use tui::widgets::Block;
-use tui::widgets::BorderType;
 use tui::widgets::Borders;
+// use tui::widgets::BorderType;
+// use tui::widgets::List;
+// use tui::widgets::ListItem;
+// use tui::widgets::ListState;
+use tui::widgets::Cell;
+use tui::widgets::Paragraph;
+use tui::widgets::Row;
+use tui::widgets::Table;
+use tui::widgets::TableState;
+use tui::widgets::Wrap;
 use tui::Frame;
 use tui::Terminal;
 
-pub struct Browse;
+pub struct Browse {
+    state: TableState,
+    listings: Vec<Listing>,
+}
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
-    loop {
-        terminal.draw(ui)?;
+impl Browse {
+    fn select_next_row(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.listings.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
 
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') | KeyCode::Esc => return Ok(()),
-                _ => {}
+    fn select_previous_row(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.listings.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn run_tui_loop<B: Backend>(&mut self, terminal: &mut Terminal<B>) -> io::Result<()> {
+        loop {
+            terminal.draw(|f: &mut Frame<B>| {
+                Browse::draw_ui(self, f);
+            })?;
+
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') | KeyCode::Esc => return Ok(()),
+                    KeyCode::Down => self.select_next_row(),
+                    KeyCode::Up => self.select_previous_row(),
+                    _ => {}
+                }
             }
         }
     }
-}
 
-fn ui<B: Backend>(f: &mut Frame<B>) {
-    let size = f.size();
+    fn draw_ui<B: Backend>(&mut self, f: &mut Frame<B>) {
+        let size = f.size();
+        let orange = Color::Rgb(255, 102, 0);
 
-    // Surrounding block
-    let block = Block::default()
-        .borders(Borders::ALL)
-        .title(
-            Span::styled("Hacker News", Style::default().fg(Color::Rgb(255, 102, 0)))
-        )
-        .title_alignment(Alignment::Left)
-        .border_type(BorderType::Rounded);
-    f.render_widget(block, size);
+        let rects = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(2)
+            // .constraints([Constraint::Percentage(10), Constraint::Percentage(90)].as_ref())
+            .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
+            .split(size);
 
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(4)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(f.size());
+        let header_layout = Layout::default()
+            .direction(Direction::Horizontal)
+            // .constraints([Constraint::Percentage(5), Constraint::Percentage(95)].as_ref())
+            .constraints([Constraint::Length(5), Constraint::Length(10)].as_ref())
+            .split(rects[0]);
 
-    // Top two inner blocks
-    let top_chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(chunks[0]);
+        let logo_box = Block::default()
+            .style(Style::default().fg(Color::White))
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(orange));
+        let logo = Paragraph::new(vec![Spans::from(Span::styled(
+            "Y",
+            Style::default().fg(Color::White),
+        ))])
+        .block(logo_box)
+        .alignment(Alignment::Center)
+        .wrap(Wrap { trim: true });
+        f.render_widget(logo, header_layout[0]);
 
-    // Top left inner block with green background
-    let block = Block::default()
-        .title(vec![
-            Span::styled("With", Style::default().fg(Color::Yellow)),
-            Span::from(" background"),
-        ])
-        .style(Style::default().bg(Color::Green));
-    f.render_widget(block, top_chunks[0]);
+        let header_box = Block::default()
+            .style(Style::default().fg(Color::White))
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(Color::Black));
+        let header = Paragraph::new(vec![Spans::from(Span::styled(
+            "Hacker News",
+            Style::default().fg(Color::White),
+        ))])
+        .block(header_box)
+        .alignment(Alignment::Left)
+        .wrap(Wrap { trim: true });
+        f.render_widget(header, header_layout[1]);
 
-    // Top right inner block with styled title aligned to the right
-    let block = Block::default()
-        .title(Span::styled(
-            "Styled title",
-            Style::default()
-                .fg(Color::White)
-                .bg(Color::Red)
-                .add_modifier(Modifier::BOLD),
-        ))
-        .title_alignment(Alignment::Right);
-    f.render_widget(block, top_chunks[1]);
+        let rows = self.listings.iter().map(|l| {
+            let cells = vec![
+                Cell::from(l.score.clone().unwrap_or(0).to_string()),
+                Cell::from(l.user.clone().unwrap_or("".to_string())),
+                Cell::from(l.title.clone()),
+            ];
 
-    // Bottom two inner blocks
-    let bottom_chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(chunks[1]);
+            Row::new(cells).height(1)
+        });
 
-    // Bottom left block with all default borders
-    let block = Block::default().title("With borders").borders(Borders::ALL);
-    f.render_widget(block, bottom_chunks[0]);
+        let block = Block::default()
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(orange));
 
-    // Bottom right block with styled left and right border
-    let block = Block::default()
-        .title("With styled borders and doubled borders")
-        .border_style(Style::default().fg(Color::Cyan))
-        .borders(Borders::LEFT | Borders::RIGHT)
-        .border_type(BorderType::Double);
-    f.render_widget(block, bottom_chunks[1]);
+        let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+
+        let table = Table::new(rows)
+            .block(block)
+            .highlight_style(selected_style)
+            .widths(&[
+                Constraint::Percentage(10),
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+            ]);
+        f.render_stateful_widget(table, rects[1], &mut self.state);
+    }
 }
 
 impl HnCommand for Browse {
@@ -123,18 +176,25 @@ impl HnCommand for Browse {
     }
 
     fn cmd(_matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
-        log::info!("Execute subcommand browse");
-        // setup terminal
+        log::info!("Start subcommand browse");
+
+        // Setup terminal
         enable_raw_mode()?;
         let mut stdout = io::stdout();
         execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
         let backend = CrosstermBackend::new(stdout);
         let mut terminal = Terminal::new(backend)?;
 
-        // create app and run it
-        let res = run_app(&mut terminal);
+        // Setup browse structure
+        let client = Client::new();
+        let listings = client.news()?;
+        let state = TableState::default();
+        let mut browse = Browse { listings, state };
 
-        // restore terminal
+        // Start tui. Infinite loop; user initiated break.
+        let res = browse.run_tui_loop(&mut terminal);
+
+        // tui loop exited; restore terminal
         disable_raw_mode()?;
         execute!(
             terminal.backend_mut(),
@@ -147,8 +207,6 @@ impl HnCommand for Browse {
             println!("{:?}", err)
         }
 
-
         Ok(())
     }
 }
-
